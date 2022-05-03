c468584093444815e88f6412d63d5018357b4f20
Thangaraj D <thangarajerode@gmail.com>
2022-05-03 22:16:18 +0530

    Code added

---
+++ ObjectDetector.py
@@ -0,0 +1,82 @@
+import cv2 as cv
+import json
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.utils.visualizer import ColorMode
+from detectron2 import model_zoo
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.modeling import build_model
+import torch
+import numpy as np
+from PIL import Image
+from com_ineuron_utils.utils import encodeImageIntoBase64
+
+
+class Detector:
+
+	def __init__(self,filename):
+
+		# set model and test set
+		self.model = 'faster_rcnn_R_50_FPN_3x.yaml'
+		self.filename = filename
+
+		# obtain detectron2's default config
+		self.cfg = get_cfg() 
+
+		# load values from a file
+		self.cfg.merge_from_file("config.yml")
+		#self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/"+self.model))
+
+		# set device to cpu
+		self.cfg.MODEL.DEVICE = "cpu"
+
+		# get weights 
+		# self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/"+self.model) 
+		#self.cfg.MODEL.WEIGHTS = "model_final_f10217.pkl"
+		self.cfg.MODEL.WEIGHTS = "model_final.pth"
+
+		# set the testing threshold for this model
+		self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.50
+        # set the class to resolve  the 'roi_heads.box_predictor.cls_score.weight'  warning
+		self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+		# build model from weights
+		#self.cfg.MODEL.WEIGHTS = self.convert_model_for_inference()
+
+	# build model and convert for inference
+	def convert_model_for_inference(self):
+
+		# build model
+		model = build_model(self.cfg)
+
+		# save as checkpoint
+		torch.save(model.state_dict(), 'checkpoint.pth')
+
+		# return path to inference model
+		return 'checkpoint.pth'
+
+
+	def inference(self, file):
+
+		predictor = DefaultPredictor(self.cfg)
+		im = cv.imread(file)
+		outputs = predictor(im)
+		print(outputs["instances"].pred_classes)
+		print(outputs["instances"].pred_boxes)
+		metadata = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).set(thing_classes=['bag', 'dress', 'hat', 'jacket', 'pants', 'shirt', 'shoe', 'shorts', 'skirt', 'sunglass'])
+		
+		# visualise
+		v = Visualizer(im[:, :, ::-1], metadata=metadata, scale=1.2)
+		v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+		predicted_image = v.get_image()
+		im_rgb = cv.cvtColor(predicted_image, cv.COLOR_RGB2BGR)
+		cv.imwrite('color_img.jpg', im_rgb)
+		# imagekeeper = []
+		opencodedbase64 = encodeImageIntoBase64("color_img.jpg")
+		# imagekeeper.append({"image": opencodedbase64.decode('utf-8')})
+		result = {"image" : opencodedbase64.decode('utf-8') }
+		return result
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Detectron2_ObjectDetection
-This repo contains local PC,Colab and flask based object detect web application based on facebook detectron2 framework
+# Detectron2 Object Detection
+This repo contains local PC,Colab and flask based object detect web application based on Facebook detection framework.
+
+##Data set:
+ https://www.kaggle.com/datasets/nguyngiabol/colorful-fashion-dataset-for-object-detection
+
+##Colab Training Procedure:
+
+1. Download the above the data set and upload the JPG folder & Json file
+2. If you have ( xml annotation file use voc2coco_predefined.py convert xml to json file format.
+3. If your planning to create own annotate data set use labeling windows software
+4. Register the dataset
+5. Download the required model file from detectron2 zoo document page link.
+6. Merge the required model config file  with base config file
+7. Download the required model checkpoint.
+8. Configure the few required parameters , like number of class
+9. Train and store the model weight into required output folder ( model_final.pth)
+10. Using saved model predict the new image for that select the few sample image & visualize.
+11. Using detectron2.utils.visualizer import ColorMode predict the object from the BW and Color images.
+12. Train with different model & continue the experiment.
+
+## Web Application:
+
+Download the pre_Trained model: https://drive.google.com/file/d/1UxXb3Z04NCQM3EIu-APWQ15GJCllA0HZ/view?usp=sharing
+
+####Use age of web application:
+
+1. Clone the repo
+2. Copy the Pre_Trained model into main folder
+3. Install the required package using requirements.txt
+4. Run python clientApp.py
+5. Open the URL in Browser
+6. Upload the sample image
+7. Press detect.
+
+
+## Result
+class_dict: {'bag': 1, 'dress': 2, 'hat': 3, 'jacket': 4, 'pants': 5, 'shirt': 6, 'shoe': 7, 'shorts': 8, 'skirt': 9, 'sunglass': 10}
+
+Detected_classes: [8, 6, 1, 6]
+
+
